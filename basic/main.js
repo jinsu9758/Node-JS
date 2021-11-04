1d34987d67631aaed194130e8832aaf804d31e41
jinsu9758 <60132287+jinsu9758@users.noreply.github.com>
2021-11-04 13:43:19 +0900

    Update main.js

--- basic/main.js
+++ basic/main.js
@@ -4,7 +4,8 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var db = require('./lib/db.js');
-var topic = require('./lib/topic');
+var topic = require('./lib/topic.js');
+var author = require('./lib/author.js');
 
 var app = http.createServer(function(request,response){
 	var _url = request.url;
@@ -44,6 +45,26 @@ var app = http.createServer(function(request,response){
 	// 삭제 과정 코드
 	else if(pathname === "/delete_process") {
 		topic.delete_process(request, response);
+	}
+	
+	else if(pathname === "/author"){
+		author.home(request, response);
+	}
+	
+	else if(pathname === "/author/create_process"){
+		author.create_author_process(request, response);
+	}
+	
+	else if(pathname === "/author/update"){
+		author.update(request, response);
+	}
+	
+	else if(pathname === "/author/update_process"){
+		author.update_process(request, response);
+	}
+	
+	else if(pathname === "/author/delete_process"){
+		author.delete_process(request, response);
 	}
 	
 	else{

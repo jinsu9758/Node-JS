f70b06bfadfdeb0bcbda91e79d06c121813b0e1c
jinsu9758 <60132287+jinsu9758@users.noreply.github.com>
2021-11-04 09:03:08 +0900

    Update template.js

--- basic/lib/template.js
+++ basic/lib/template.js
@@ -30,5 +30,18 @@ module.exports = {
 		list = list + '</ul>';
 
 		return list;
+	},
+	authorSelect : function(authors, author_id){
+		var tag='';
+		var i =0;
+		while(i < authors.length){
+			var selected = '';
+			if(authors[i].id === author_id){
+				selected = ' selected'
+			}
+			tag = tag + `<option value="${authors[i].id}" ${selected}>${authors[i].name}</option>`;
+			i++;
+		}
+		return tag;
 	}
 }

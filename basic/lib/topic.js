80a607ce18c3be51bde0bd7799df402e183a48f4
jinsu9758 <60132287+jinsu9758@users.noreply.github.com>
2021-11-04 09:52:10 +0900

    Update topic.js

--- basic/lib/topic.js
+++ basic/lib/topic.js
@@ -1,5 +1,9 @@
+// 기능 부분을 담당하고 있음.
+
 var db = require('./db.js');
 var template = require('./template.js');
+var url = require('url');
+var qs = require('querystring');
 
 exports.home = function(request, response){
 	db.query("select * from topic", function(error, topics){
@@ -12,3 +16,172 @@ exports.home = function(request, response){
     response.end(html);
 	});
 }
+
+
+exports.page = function(request, response){
+	var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+	db.query("select * from topic", function(error, topics){
+		if(error){
+			throw error;
+		}
+		db.query(`select * from topic left join author on topic.author_id = author.id where topic.id=?`, [queryData.id], function(error2, topic){
+			if(error2){
+				throw error2;
+			}
+			console.log(topic);
+			var title = topic[0].title;
+			var description = topic[0].description;
+			var list = template.list(topics);
+			var html = template.HTML(title, list, `<h2>${description}</h2> <p>by ${topic[0].name}</p>`, `<a href="/create">create</a>
+                <a href="/update?id=${queryData.id}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${queryData.id}">
+                  <input type="submit" value="delete">
+                </form>`);
+			response.writeHead(200);
+			response.end(html);
+		});
+	});
+}
+
+
+exports.create = function(request, response){
+	db.query("select * from topic", function(error, topics){
+		//console.log(topics);
+		db.query(`select * from author`, function(error2, authors){
+			var tag = template.authorSelect(authors);
+			var title = 'Create';
+			var list = template.list(topics);
+			var html = template.HTML(title, list, `
+			<form action="/create_process" method="post">
+					<p>
+					<input type="text" name="title" placeholder="title">
+					</p>
+					<p>
+						<textarea name="description" placeholder="description"></textarea>
+					</p>
+					<p>
+					<select name="author">
+						${tag};
+					</select>
+					</p>
+					<p>
+						<input type="submit" name="submit">
+					</p>
+			</form>`, `<a href="/create">create</a>`);
+			response.writeHead(200);
+			response.end(html);
+		});
+	});
+}
+
+
+exports.create_process = function(request, response){
+	var body = '';
+		
+	request.on('data', function(data){
+		body += data;
+	});
+		
+	//들어올 data가 없으면 요고 실행
+	request.on('end', function(){
+		var post = qs.parse(body); //post 정보 저장
+		db.query(`insert into topic(title, description,created, author_id) values(?, ?, NOW(), ?)`, [post.title, post.description, post.author], function(error, result){
+				if(error){
+					throw error;
+				}
+				response.writeHead(302, {Location:`/?id=${result.insertId}`});
+				response.end();
+		})
+	});
+}
+
+
+exports.update = function(request, response){
+	var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+	db.query(`select * from topic`, function(error, topics){
+		if(error){
+			throw error;
+		}
+		db.query(`select * from topic where id=?`, [queryData.id], function(error2, topic){
+			if(error2){
+				throw error2;
+			}
+			db.query(`select * from author`, function(error2, authors){
+				var tag = template.authorSelect(authors, topic[0].author_id);
+				var list = template.list(topics);
+				var html = template.HTML(topic[0].title, list, `
+				<form action="/update_process" method="post">
+					<input type="hidden" name="id" value="${topic[0].id}">
+					<p>
+					<input type="text" name="title" placeholder="title" value="${topic[0].title}"> // 수정된 후
+					</p>
+					<p>
+						<textarea name="description" placeholder="description">${topic[0].description}</textarea>
+					</p>
+					<p>
+					<select name="author">
+						${tag};
+					</select>
+					</p>
+					<p>
+						<input type="submit" name="submit">
+					</p>
+				</form>`, `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`)
+				response.writeHead(200);
+				response.end(html);
+			});
+		});
+	});	
+}
+
+
+exports.update_process = function(request, response){
+	var body = '';
+		
+	request.on('data', function(data){
+		body += data;
+	});
+	
+	//들어올 data가 없으면 요고 실행
+	request.on('end', function(){
+		var post = qs.parse(body); //post 정보 저장
+		db.query(`update topic set title=?, description=?, author_id=? where id=?`, [post.title, post.description,post.author, post.id], function(error, result){
+			if(error){
+				throw error;
+			}
+			response.writeHead(302, {Location:`/?id=${post.id}`});
+			response.end();
+		})
+	});
+}
+
+exports.delete_process = function(request, response){
+	var body = '';
+	
+	request.on('data', function(data){
+		body += data;
+	});
+	
+	//들어올 data가 없으면 요고 실행
+	request.on('end', function(){
+		var post = qs.parse(body); //post 정보 저장
+		//console.log(post.title);
+		//console.log(post.description);
+		var id = post.id;
+		db.query(`delete from topic where id=?`,[id], function(error, result){
+			if(error){
+				throw error;
+			}
+			response.writeHead(302, {Location:`/`});
+			response.end();
+		});
+	});
+}
+
+
+
